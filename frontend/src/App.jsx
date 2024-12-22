4e084d983f94fbc4c5c4e025b1c5d4ce681afa4e
Irina Vadimovna <evdokimovaira2000@mail.ru>
2024-12-22 17:34:47 +0700

    step 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './slices/AuthContext.jsx';
 import { General } from './components/General.jsx';
 import { Login } from './components/Login.jsx';
 import { NotFound } from './components/NotFoundPage.jsx';
 
+
 function App() {
+  const isAuthenticated = !!localStorage.getItem('token');
   return (
+    <AuthProvider>
       <Routes>
-        <Route path="/" element={<General />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={isAuthenticated ? <General /> : <Navigate to="/login" />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      </AuthProvider>
   );
 }
 
