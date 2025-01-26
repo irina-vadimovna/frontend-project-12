630bfe24e8471d71b0244be70a105c130a4b19bc
Irina Vadimovna <evdokimovaira2000@mail.ru>
2025-01-26 21:08:54 +0700

    fix 3

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,21 +5,21 @@ import pluginReact from 'eslint-plugin-react';
 export default [
   { files: ['**/*.{js,jsx}'] },
   { ignores: ['/node_modules/', 'dist/'] },
-  { 
+  {
     languageOptions: {
-    ecmaVersion: 'latest',
-    globals: globals.browser,
-    sourceType: 'module',
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      sourceType: 'module',
     },
   },
   { settings: { react: { version: '18.3' } } },
   pluginReact.configs.flat.recommended,
-  { 
+  {
     rules:
-      { 
+      {
         semi: 'error',
         'react/react-in-jsx-scope': 'off',
-        'react/prop-types': 'off' 
+        'react/prop-types': 'off',
       },
   },
 ];
